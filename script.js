0ba530eac7f64deaa6be0727f01ca208bde192c1
Andre Hemerly <andrehemerlybr123brh@gmail.com>
2026-03-10 11:37:17 -0300

    Animação ao tocar nos botões e wallpaper automático que troca o background automaticamente com os projetos da Qasa 3d

--- script.js
+++ script.js
@@ -42,3 +42,46 @@ const observer = new IntersectionObserver(function(entries) {
 elements.forEach(function(el) {
   observer.observe(el)
 })
+
+const vendas = document.querySelector('#vendas')
+
+const wallpapers = [
+  'assets/img-projeto/projeto1.jpeg',
+  'assets/img-projeto/projeto2.jpeg',
+  'assets/img-projeto/projeto3.jpeg',
+  'assets/img-projeto/projeto4.jpeg',
+  'assets/img-projeto/projeto5.jpeg'
+]
+
+let indiceAtual = 0
+
+function trocarWallpaper() {
+  const proximoIndice = (indiceAtual + 1) % wallpapers.length
+
+  const img = new Image()
+  img.src = wallpapers[proximoIndice]
+
+  function realizarTroca() {
+    vendas.classList.add('transitando')
+
+    setTimeout(function() {
+      indiceAtual = proximoIndice
+      vendas.style.backgroundImage = `url('${wallpapers[indiceAtual]}')`
+      vendas.style.backgroundSize = 'cover'
+      vendas.style.backgroundPosition = 'center'
+      vendas.classList.remove('transitando')
+    }, 500)
+  }
+
+  if (img.complete) {
+    realizarTroca()
+  } else {
+    img.onload = realizarTroca
+  }
+}
+
+vendas.style.backgroundImage = `url('${wallpapers[0]}')`
+vendas.style.backgroundSize = 'cover'
+vendas.style.backgroundPosition = 'center'
+
+setInterval(trocarWallpaper, 4000)
